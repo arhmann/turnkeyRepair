ca2e73ad7fc801a7fe2cbd3676eadb4dc2676c46
iurii.a <iu.arhmann@gmail.com>
2020-11-01 15:43:26 +0300

    added slider photo

--- src/js/main.js
+++ src/js/main.js
@@ -1,41 +1,33 @@
-'use strict';
+"use strict";
 
-// Загрузка библиотек
-// require('slick-carousel');
+const buttons = document.querySelectorAll(".nav-items-btn");
+const sliders = document.querySelectorAll(".slide");
+const btnLeft = document.getElementById("item-1")
+const btnMiddle = document.getElementById("item-2")
+const btnRight = document.getElementById("item-3")
 
+buttons.forEach((btn) =>
+  btn.addEventListener("click", (event) => {
+    event.preventDefault();
 
-(function($) {
-  const App = {
-    init: function() {
-      console.time('Time');
-      const that = this;
-      that.test();
-      that.resize(that);
-      console.timeEnd('Time');
-    },
-    test: function() {
-      const $btn = $('.test');
-      if ($btn.length) {
-        $btn.on('click', function() {
-          const self = $(this);
-          console.log('test');
-        })
+    sliders.forEach((slide) => {
+      slide.classList.add("not-active");
+
+      if (event.target.attributes[1].name == "data-btnleft") {
+        btnLeft.classList.add("active")
+        btnMiddle.classList.remove("active");
+        btnRight.classList.remove("active");
+      } else if (event.target.attributes[1].name == "data-btnmiddle"){
+        btnMiddle.classList.add("active")
+        btnLeft.classList.remove("active")
+        btnRight.classList.remove("active");
+      } else if (event.target.attributes[1].name == "data-btnright"){
+        btnRight.classList.add("active")
+        btnMiddle.classList.remove("active")
+        btnLeft.classList.remove("active")
+
       }
-    },
 
-    resize: function (that) {
-      let timer = 0;
-      $(window).on('resize', function () {
-        clearTimeout(timer);
-        timer = setTimeout(function () {
-
-          console.log('resize');
-        }, 250);
-      });
-    },
-  };
-  App.init();
-
-})(jQuery);
-
-
+    });
+  })
+);
